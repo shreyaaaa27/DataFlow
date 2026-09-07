94cc551ae6043cd466f7c09b28e5b113914a4f53
shreyaaaa27 <shreyya.singh27@gmail.com>
2026-09-07 11:59:25 +0530

    Airflow Setup with hello-world DAG

---
+++ dags/hello_world_dag.py
@@ -0,0 +1,19 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from datetime import datetime
+
+
+def say_hello():
+    print("Hello from DataFlow's first DAG!")
+
+
+with DAG(
+    dag_id="hello_world",
+    start_date=datetime(2026, 1, 1),
+    schedule="@daily",
+    catchup=False,
+) as dag:
+    task = PythonOperator(
+        task_id="say_hello",
+        python_callable=say_hello,
+    )
